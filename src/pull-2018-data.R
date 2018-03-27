6b5f3cd7488177bb908b78df0f59a5a32c88c244
Andrew Borgman <ndrwsbrgmn@gmail.com>
2018-03-27 22:13:56 +0000

    add in 2018 roto info

--- src/pull-2018-data.R
+++ src/pull-2018-data.R
@@ -18,36 +18,53 @@ player_ids <- do.call(plyr::rbind.fill, lapply(pos, function(p) {
   sel <- grep("MLBAMID", vl %>% html_attr('href'))
   nms <- vl[sel] %>% html_text() %>% parse_names()
   nms$MLBAMID <- vl[sel] %>% html_attr('href') %>% gsub("^[^=]+|=", "", .) 
+  nms$POS <- p
   nms
 }))
 
 # scrape all the available projections for each player
 proj <- lapply(1:nrow(player_ids), function(i) {
   message(player_ids$fullName[i])
-  
-  u <- sprintf("https://rotochamp.com/Baseball/Player.aspx?MLBAMID=%s", player_ids$MLBAMID[i])
-  pg <- read_html(u)
-  prj <- pg %>% 
-    html_node('#MainContent_gridHitterProjections') %>% 
-    html_table()
-  prj <- prj[prj[, 1] != 2017,]
-  
 
-  u2 <- sprintf("https://rotochamp.com/Baseball/PlayerPositions.aspx?MLBAMID=%s", player_ids$MLBAMID[i])
-  pg2 <- read_html(u2)
-  ps <- pg2 %>% 
-    html_node('#MainContent_gridGamesPlayed_Primary') %>% 
-    html_table()
+  cname <- ""  
+
+  if (player_ids$POS[i] %in% c('SP', 'RP')) {
+    cname <- "#MainContent_gridPitcherProjections"
+  } else {
+    cname <- "#MainContent_gridHitterProjections"
+  }
   
-  list(
+  e <- try({
+    u <- sprintf("https://rotochamp.com/Baseball/Player.aspx?MLBAMID=%s", player_ids$MLBAMID[i])
+    pg <- read_html(u)
+    prj <- pg %>% 
+      html_node(cname) %>% 
+      html_table()
+    prj <- prj[prj[, 1] != 2017,]
+    
+  
+    u2 <- sprintf("https://rotochamp.com/Baseball/PlayerPositions.aspx?MLBAMID=%s", player_ids$MLBAMID[i])
+    pg2 <- read_html(u2)
+    ps <- pg2 %>% 
+      html_node('#MainContent_gridGamesPlayed_Primary') %>% 
+      html_table()
+    })
+  if (inherits(e, 'try-error')) prj <- ps <- NULL
+  
+  Sys.sleep(.5)
+  return(list(
     player_ids[i,],
     prj,
     ps
-  )
-  Sys.sleep(.5)
+  ))
   
 })
 
-saveRDS('projections/2018/2018-projections-parsed.rds')
+saveRDS(object = proj, file = 'projections/2018/2018-projections-parsed.rds')
+
+nnul <- sapply(proj, function(x) !is.null(x[[2]]))
+
+proj <- proj[nnul]
+saveRDS(object = proj, file = 'projections/2018/2018-projections-parsed-clean.rds')
 
 
